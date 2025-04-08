65d424a58295f8e38c36ea354ff09d8bab29ea7e
Raccoon <ikhode.station@gmail.com>
2025-04-08 09:26:53 +0700

    set-up installation

---
+++ my-project/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+
+</script>
+
+<template>
+  <h1 class="text-3xl text-yellow-400 font-bold underline">
+    Hello world!
+  </h1>
+</template>
